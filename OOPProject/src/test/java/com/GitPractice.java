176ce9f72bb89f2c800b464b9a83ae93533caf9b
Bittech Consulting <bittechconsulting@bittechs-imac-5.lan>
2019-03-02 14:01:47 -0500

    sad

---
+++ OOPProject/src/test/java/com/GitPractice.java
@@ -0,0 +1,30 @@
+package com;
+
+public class GitPractice 
+{
+	/*How to export a project from eclipse to Repo
+	 * 1. right click on project-->export-->file system-->next--->browse
+	 * upto local repo--->open-->finish
+	 * 
+	 * How to import in eclipse
+	 * right click--->import-->existing maven project
+	 * --->browse repo-->open-->finish
+	 * */
+	
+	
+	/*Some linux command
+	 * cd--change directory
+	 * pwd--to see directory label
+	 * ls--to see file
+	 * mkdir--create a folder
+	 * touch--to create a file
+	 * vi/nano----editor
+	 * ps -ax--to see how many application is on
+	 * kill---to close an application based on PID number
+	 * grep---to get a string value from a file
+	 * 
+	 * 
+	 * 
+	 * */
+
+}
